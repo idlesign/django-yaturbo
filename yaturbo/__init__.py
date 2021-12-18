0c42aa5e22ec9962c203233a29dd00903d976af4
idle sign <idlesign@yandex.ru>
2021-12-18 11:22:57 +0700

    Release v1.0.1

--- yaturbo/__init__.py
+++ yaturbo/__init__.py
@@ -1,7 +1,7 @@
 from .toolbox import YandexTurboFeed, sanitize_turbo
 
 
-VERSION = (1, 0, 0)
+VERSION = (1, 0, 1)
 """Application version number tuple."""
 
 VERSION_STR = '.'.join(map(str, VERSION))
